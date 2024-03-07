5d0f7da2a97a6ada9c1acf52af792997c00e7bb6
TianyuLinvictor <ltylin@ucdavis.edu>
2024-03-07 12:27:22 -0800

    up date

---
+++ homework/hw13/Q2.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+
+UC_admit <- read_csv("data/UC_admit.csv")
+
+UC_admit<- UC_admit %>%
+  filter(FilteredCountFR  !="NA")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "UC Campus Admissions by Ethnicity 2010-2019"),
+  dashboardSidebar(disable=T),
+  dashboardBody(
+    fluidRow(
+      box(title = "Plot Options", width = 3,
+          radioButtons("x", "Select Year", choices = c("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"), 
+                       selected = "2010"),
+          selectInput("y", "Select Campus", choices = c("Davis", "Irvine", "Berkeley", "Irvine", "Los_Angeles", "Merced", "Riverside", "San_Diego", "Santa_Barbara", "Santa_Cruz"),
+                      selected = "Davis"),
+          selectInput("z", "Select Admit Category", choices = c("Applicants", "Admits", "Enrollees"),
+                      selected = "Applicants")
+      ), # close the first box
+      box(title = "UC Admissions", width = 8,
+          plotOutput("plot", width = "600px", height = "500px")
+      ) # close the second box
+    ) # close the row
+  ) # close the dashboard body
+) # close the ui
+
+server <- function(input, output, session) { 
+  
+  output$plot <- renderPlot({
+    UC_admit %>% 
+      filter(Academic_Yr==input$x & Campus==input$y & Category==input$z) %>% 
+      ggplot(aes(x=reorder(Ethnicity, FilteredCountFR), y=FilteredCountFR)) + 
+      geom_col(color="black", fill="green", alpha=0.75) +
+      theme_light(base_size = 18) +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))+
+      labs(x = "Ethnicity", y = "Number")
+  })
+  
+  session$onSessionEnded(stopApp)
+  
+}
+
+shinyApp(ui, server) 
